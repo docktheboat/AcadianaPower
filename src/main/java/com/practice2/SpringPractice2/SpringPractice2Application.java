8b551eb506b9d6049b18bc3539fddecc1b2c443c
docktheboat <patrickedgarcourts@yahoo.com>
2022-01-22 01:20:46 -0600

    stop deployment

--- src/main/java/com/practice2/SpringPractice2/SpringPractice2Application.java
+++ src/main/java/com/practice2/SpringPractice2/SpringPractice2Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringPractice2Application extends SpringBootServletInitializer {
+public class SpringPractice2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringPractice2Application.class, args);

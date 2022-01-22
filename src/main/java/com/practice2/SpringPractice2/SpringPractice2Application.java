c6394358d3d54c2049ea9e62a76c646c831a733e
docktheboat <patrickedgarcourts@yahoo.com>
2022-01-22 00:48:26 -0600

    heroku deployment fix

--- src/main/java/com/practice2/SpringPractice2/SpringPractice2Application.java
+++ src/main/java/com/practice2/SpringPractice2/SpringPractice2Application.java
@@ -2,9 +2,10 @@ package com.practice2.SpringPractice2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringPractice2Application {
+public class SpringPractice2Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringPractice2Application.class, args);

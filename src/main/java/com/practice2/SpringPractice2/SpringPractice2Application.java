a448549bc153467008d9c42110a934d7519ff646
docktheboat <patrickedgarcourts@yahoo.com>
2022-01-21 19:23:33 -0600

    first

---
+++ src/main/java/com/practice2/SpringPractice2/SpringPractice2Application.java
@@ -0,0 +1,13 @@
+package com.practice2.SpringPractice2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPractice2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPractice2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice2/SpringPractice2/SpringPractice2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice2.SpringPractice2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPractice2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

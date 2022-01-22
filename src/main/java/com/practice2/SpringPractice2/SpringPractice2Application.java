3d447789dce64ccb204f906a4973b410ded47b2c
docktheboat <patrickedgarcourts@yahoo.com>
2022-01-22 01:52:34 -0600

    fix

--- src/main/java/com/practice2/SpringPractice2/SpringPractice2Application.java
+++ src/main/java/com/practice2/SpringPractice2/SpringPractice2Application.java
@@ -1,10 +1,15 @@
 package com.practice2.SpringPractice2;
 
+import com.practice2.SpringPractice2.student.StudentModel;
+import com.practice2.SpringPractice2.student.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
 public class SpringPractice2Application {
@@ -17,8 +22,16 @@ public class SpringPractice2Application {
 }
 @RestController
 class Welcome{
+
+	private final StudentService studentService;
+
+	@Autowired
+	public Welcome(StudentService studentService) {
+		this.studentService = studentService;
+	}
+
 	@GetMapping("/")
-	public String welcome(){
-		return "Welcome";
+	public List<StudentModel> welcome(){
+		return studentService.getAllStudents();
 	}
 }

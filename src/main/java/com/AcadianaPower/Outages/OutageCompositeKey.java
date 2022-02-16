515eef2fb2a88b8486618d96e6d17b5cbffa6921
docktheboat <patrickedgarcourts@yahoo.com>
2022-02-15 23:07:20 -0600

    Modifying outages to use two primary key columns outageType and zipCode

---
+++ src/main/java/com/AcadianaPower/Outages/OutageCompositeKey.java
@@ -0,0 +1,8 @@
+package com.AcadianaPower.Outages;
+
+import java.io.Serializable;
+
+public class OutageCompositeKey implements Serializable {
+    private String outageType;
+    private Integer zipCode;
+}

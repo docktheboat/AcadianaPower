674707dcce02d6859625f8b6506c0ee08cdfccbf
docktheboat <patrickedgarcourts@yahoo.com>
2022-03-13 19:56:33 -0500

    Setting angular url as env var

--- src/main/java/com/AcadianaPower/Config/WebConfig.java
+++ src/main/java/com/AcadianaPower/Config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.AcadianaPower.Config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -9,12 +10,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Configuration
 public class WebConfig {
 
+    @Value("${angular.url}")
+    private String url;
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurerAdapter() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+                registry.addMapping("/**").allowedOrigins(url);
             }
         };
     }

afc03e15dea270a15db9ec9d7ffd092f02e6627f
docktheboat <patrickedgarcourts@yahoo.com>
2022-03-21 17:28:01 -0500

    Edit cross origin in customer controller

--- src/main/java/com/AcadianaPower/Controllers/CustomerController.java
+++ src/main/java/com/AcadianaPower/Controllers/CustomerController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
-@CrossOrigin(origins = "${angular.url}")
+@CrossOrigin(origins = "${angular.url}" + "/Customer")
 @RestController
 @RequestMapping(path = "/Customer")
 public class CustomerController {

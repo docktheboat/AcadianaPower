faee6db14370c5112e2ee5552b040a0340c3a0ad
docktheboat <patrickedgarcourts@yahoo.com>
2022-03-22 10:41:30 -0500

    Adding company email as local var in email controller

--- src/main/java/com/AcadianaPower/Controllers/EmailController.java
+++ src/main/java/com/AcadianaPower/Controllers/EmailController.java
@@ -3,6 +3,7 @@ package com.AcadianaPower.Controllers;
 import com.AcadianaPower.Models.EmailModel;
 import com.AcadianaPower.Services.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/Email")
 public class EmailController {
 
+    @Value("${spring.mail.username}")
+    private String companyEmail;
 
     @Autowired
     private JavaMailSender javaMailSender;
@@ -19,8 +22,8 @@ public class EmailController {
     @PostMapping("/send")
     public void sendEmail(@RequestBody EmailModel emailModel){
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom(EmailService.companyEmail);
-        message.setTo(EmailService.companyEmail);
+        message.setFrom(emailModel.getSender());
+        message.setTo(companyEmail);
         message.setText("From " + emailModel.getSender() +": " +emailModel.getMessage());
         message.setSubject("Outage Report " +
                 emailModel.getService() + " "

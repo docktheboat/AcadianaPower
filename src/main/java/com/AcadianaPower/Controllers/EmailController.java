d3102566d28d43b88c8bd2d57515df776944f698
docktheboat <patrickedgarcourts@yahoo.com>
2022-03-22 08:00:39 -0500

    Adding email controller for customers to send emails to company

---
+++ src/main/java/com/AcadianaPower/Controllers/EmailController.java
@@ -0,0 +1,29 @@
+package com.AcadianaPower.Controllers;
+
+import com.AcadianaPower.Models.EmailModel;
+import com.AcadianaPower.Services.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/Email")
+public class EmailController {
+
+    public final EmailService emailService;
+
+    @Autowired
+    public EmailController(EmailService emailService){
+        this.emailService = emailService;
+    }
+
+
+    @PostMapping("/send")
+    public void reportOutage(@RequestBody EmailModel emailModel){
+        emailService.sendEmail(
+                emailModel.getSender(),
+                EmailService.companyEmail,
+                "Outage Report (" + emailModel.getService() +"," + emailModel.getZipCode()+")",
+                emailModel.getMessage()
+        );
+    }
+}

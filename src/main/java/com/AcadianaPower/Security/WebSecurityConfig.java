e9055cda98d752489f2f9a960ed7dae01e80b79c
docktheboat <patrickedgarcourts@yahoo.com>
2022-04-05 22:19:16 -0500

    Changing authed path to frontend

--- src/main/java/com/AcadianaPower/Security/WebSecurityConfig.java
+++ src/main/java/com/AcadianaPower/Security/WebSecurityConfig.java
@@ -24,7 +24,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests()
                 .antMatchers(
-                        "/**"
+                        "${angular.url}/**"
                 ).permitAll()
                 .anyRequest().authenticated()
                 .and().formLogin();

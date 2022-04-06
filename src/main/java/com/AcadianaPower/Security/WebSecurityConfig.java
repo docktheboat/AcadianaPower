89786f4e282a5086882b7e18568be1af0afb9ec3
docktheboat <patrickedgarcourts@yahoo.com>
2022-04-05 21:58:29 -0500

    Require login for all paths except registration

--- src/main/java/com/AcadianaPower/Security/WebSecurityConfig.java
+++ src/main/java/com/AcadianaPower/Security/WebSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -23,10 +24,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests()
                 .antMatchers(
-                        "/${angular.url}/**"
+                        "/**"
                 ).permitAll()
                 .anyRequest().authenticated()
                 .and().formLogin();
+    }
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/register");
     }
 
     @Override
